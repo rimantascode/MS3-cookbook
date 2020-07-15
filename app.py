5de651103bbeb11557480c41b59a0b2571fe5568
rimantascode <r.preiks@gmail.com>
2020-07-15 12:39:44 +0000

    init commitemtent and conected to the database and tested

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/index')
+def get_tasks():
+    return render_template("index.html",
+                           recipe=mongo.db.recipe.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
