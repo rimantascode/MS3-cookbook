72a295c0d472a5f279988ca837aec67fafba1b44
rimantascode <r.preiks@gmail.com>
2020-08-01 21:47:07 +0000

    removed nav-bar subfolders and added a card in searrch list section

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app)
 @app.route('/index')
 def get_tasks():
     return render_template("index.html",
-                           recipe=mongo.db.difficulty.find())
+                           recipe=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
