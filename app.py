6e7e09556ebc7d5acee6eda6a778acbca20c65d2
rimantascode <r.preiks@gmail.com>
2020-08-16 16:06:02 +0000

    dish.html is wiered up and running

--- app.py
+++ app.py
@@ -30,28 +30,28 @@ def edit_recipe(recipes_id):
 @app.route('/dish/<recipes_id>')
 def dish(recipes_id):
     the_recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipes_id)})
+    list = mongo.db.recipe.find()
     all_categories = mongo.db.categories.find()
+    for s in list:
+        v = s["ingrediants"].split(",")
     return render_template('dish.html', recipes=the_recipe,
-                           categories=all_categories)
-    """
-    return render_template("dish.html", recipe=mongo.db.recipe.find(), description=mongo.db.recipe.find(), image=mongo.db.recipe.find())
-    """
+                           categories=all_categories,  image=mongo.db.recipe.find(), ingrediants=v)
 
 
-@app.route('/add_recipe')
+@ app.route('/add_recipe')
 def add_recipe():
     return render_template('add_recipe.html',
                            categ=mongo.db.difficulty.find(), time=mongo.db.prep_time.find())
 
 
-@app.route('/insert_task', methods=['POST'])
+@ app.route('/insert_task', methods=['POST'])
 def insert_recipe():
     tasks = mongo.db.recipe
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
 
-@app.route('/update_task/<recipes_id>', methods=["POST"])
+@ app.route('/update_task/<recipes_id>', methods=["POST"])
 def update_recipe(recipes_id):
     recipe = mongo.db.recipe
     recipe.update({'_id': ObjectId(recipes_id)},
@@ -63,6 +63,12 @@ def update_recipe(recipes_id):
         'picture_url': request.form.get('picture_url'),
         "ingrediants": request.form.get('ingrediants')
     })
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/delete_recipe/<recipes_id>')
+def delete_recipe(recipes_id):
+    mongo.db.recipe.remove({'_id': ObjectId(recipes_id)})
     return redirect(url_for('get_tasks'))
 
 
